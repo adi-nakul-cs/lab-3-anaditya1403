966766b45108da7aa1f0253d4ebc83c92d184827
Aditya Ashok Naik <muddu1403@gmail.com>
2025-02-20 00:29:34 +0530

    array123 with test cases.
    done.

--- array123.py
+++ array123.py
@@ -1,6 +1,19 @@
-# Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+# Given an array of ints, return True if the sequence of numbers 1, 2, 3
+#  appears in the array somewhere.
 
 
 # array123([1, 1, 2, 3, 1]) → True
 # array123([1, 1, 2, 4, 1]) → False
 # array123([1, 1, 2, 1, 2, 3]) → True
+
+def array123(num):
+    for i in range(len(num) - 2):
+        if num[i] == 1 and num[i + 1] == 2 and num[i + 2] == 3:
+            return True
+        
+    else:
+        return False
+        
+
+if __name__ == "__main__":
+    print(array123([4,1,4,3,2,1,2,3,4,5,6]))
--- test_array123.py
+++ test_array123.py
@@ -3,13 +3,20 @@ from array123 import array123
 
 class TestArray123(unittest.TestCase):
     def test_array123_123(self):
-        self.assertEqual(array123([1,1,2,3,1]), True)
-        self.assertEqal([1,1,2,4,1], False)
-        self.assertEqual(array123([1,1,2,1,2,3]), True)
-        self.assertEqual(array123([1,1,2,1,2]), False)
-        self.assertEqual(array123([1,2,3,1,2,3]), True)
-        self.assertEqual(array123([1,2,3]), True)
-        self.assertEqual(array123([1,1,1]), False)
-        self.assertEqual(array123([1,2]), False)
-        self.assertEqual(array123([1]), False)
-        self.assertEqual(array123([]), False)
+        try :
+            self.assertEqual(array123([1,1,2,3,1]), True)
+            self.assertEqual(array123([1,1,2,4,1]), False)
+            self.assertEqual(array123([1,1,2,1,2,3]), True)
+            self.assertEqual(array123([1,1,2,1,2]), False)
+            self.assertEqual(array123([1,2,3,1,2,3]), True)
+            self.assertEqual(array123([1,2,3]), True)
+            self.assertEqual(array123([1,1,1]), False)
+            self.assertEqual(array123([1,2]), False)
+            self.assertEqual(array123([1]), False)
+            self.assertEqual(array123([]), False)
+            print(f"Success!")
+        except AssertionError as e :
+            print(f"{e}")
+
+if __name__ == "__main__":
+    unittest.main()
