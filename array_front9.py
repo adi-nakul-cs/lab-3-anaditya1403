50c71dc722414614dcfd92a68f61ab5510ec1981
Aditya Ashok Naik <muddu1403@gmail.com>
2025-02-20 00:49:06 +0530

    Array Front 9
    First 4 elements of array == 9 or not;
    
    looping pending

--- array_front9.py
+++ array_front9.py
@@ -1,6 +1,20 @@
-# Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+# Given an array of ints, return True if one of the first 4 elements in the array is a 9. 
+# The array length may be less than 4.
 
 
 # array_front9([1, 2, 9, 3, 4]) → True
 # array_front9([1, 2, 3, 4, 9]) → False
 # array_front9([1, 2, 3, 4, 5]) → False
+
+def array_front9(nums):
+  for i in range(len(nums) - 4):
+    if nums[i] == 9 or nums[i + 1] == 9 or nums[i + 2] == 9 or nums[i + 3] == 9:
+      return True
+      
+  else:
+    return False
+    
+if __name__ == "__main__":
+  print(array_front9([4,8,9,9,8]))
+
+#Code not yet Completed/...
