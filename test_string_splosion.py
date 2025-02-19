2e2eb2c158be103f25df1dce8867f9b63bc97cd7
Aditya Ashok Naik <muddu1403@gmail.com>
2025-02-19 23:54:43 +0530

    String splosion and test for string splosion
    String splosion and test cases.
    no assertion has failed..

--- test_string_splosion.py
+++ test_string_splosion.py
@@ -3,13 +3,21 @@ from string_splosion import string_splosion
 
 class TestStringSplosion(unittest.TestCase):
     def test_string_splosion(self):
-        self.assertEqual(string_splosion('Code'), 'CCoCodCode')
-        self.assertEqual(string_splosion('abc'), 'aababc')
-        self.assertEqual(string_splosion('ab'), 'aab')
-        self.assertEqual(string_splosion('x'), 'x')
-        self.assertEqual(string_splosion('fade'), 'ffafadfade')
-        self.assertEqual(string_splosion('There'), 'TThTheTherThere')
-        self.assertEqual(string_splosion('Kitten'), 'KKiKitKittKitteKitten')
-        self.assertEqual(string_splosion('Bye'), 'BByBye')
-        self.assertEqual(string_splosion('Good'), 'GGoGooGood')
-        self.assertEqual(string_splosion('Bad'), 'BBaBad')
+        try :
+            self.assertEqual(string_splosion('Code'), 'CCoCodCode')
+            self.assertEqual(string_splosion('abc'), 'aababc')
+            self.assertEqual(string_splosion('ab'), 'aab')
+            self.assertEqual(string_splosion('x'), 'x')
+            self.assertEqual(string_splosion('fade'), 'ffafadfade')
+            self.assertEqual(string_splosion('There'), 'TThTheTherThere')
+            self.assertEqual(string_splosion('Kitten'), 'KKiKitKittKitteKitten')
+            self.assertEqual(string_splosion('Bye'), 'BByBye')
+            self.assertEqual(string_splosion('Good'), 'GGoGooGood')
+            self.assertEqual(string_splosion('Bad'), 'BBaBad')
+            print("All the assertions in this block run sucessfully.")
+
+        except AssertionError as e:
+            print(f"Asseertion has failed{e}")
+
+if __name__ == "__main__":
+    unittest.main()
